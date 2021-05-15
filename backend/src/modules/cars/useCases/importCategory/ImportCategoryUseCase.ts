6d25b10234fbe1925702c179aaded394c0c21b4b
Matheus Thomaz <matheustthomaz@gmail.com>
2021-05-15 00:24:19 -0300

    New use cases added on category section. Now its possible to create, list and import categories from .csv files

---
+++ backend/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,67 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+import { inject, injectable } from "tsyringe";
+
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+@injectable()
+class ImportCategoryUseCase {
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) {}
+
+  loadCategoriesFromFile(
+    file: Express.Multer.File
+  ): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const fsReadStream = fs.createReadStream(file.path);
+
+      const categories: IImportCategory[] = [];
+
+      const csvParseFile = csvParse();
+
+      fsReadStream.pipe(csvParseFile);
+
+      csvParseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const loadCategories = await this.loadCategoriesFromFile(file);
+
+    loadCategories.map(async (category) => {
+      const { name, description } = category;
+
+      const existCategory = await this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        await this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
+    });
+  }
+}
+
+export { ImportCategoryUseCase };
