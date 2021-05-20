91947db35925c2c38967684c4a7b7a233a731d71
Matheus Thomaz <matheustthomaz@gmail.com>
2021-05-19 23:57:21 -0300

    New adjustments added into app file, such as rate limiter and Sentry integration.

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -11,12 +11,56 @@ import * as Tracing from "@sentry/tracing";
 
 import "@shared/container";
 
+import { AppError } from "@shared/errors/AppError";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
+import { router } from "@shared/infra/http/routes";
 import createConnection from "@shared/infra/typeorm";
+
+import swaggerFile from "../../../swagger.json";
 
 createConnection();
 const app = express();
 
+app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+
 app.use(cors());
+app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 export { app };
